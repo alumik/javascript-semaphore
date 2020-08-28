2260d703669205f34196c3146364371e342b37aa
Zhong Zhenyu <nczzy1997@gmail.com>
2020-08-28 19:29:40 +0800

    添加展示材料

--- sample/js/bad_worker.js
+++
@@ -1,7 +0,0 @@
-onmessage = msg => {
-    const index = msg.data.index
-    const color = msg.data.color
-    for (let i = 0; i < 20; i++) {
-        console.log('%c' + index + ' - ' + i, 'color:' + color)
-    }
-}
